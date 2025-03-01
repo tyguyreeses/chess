390ad333d96a31e24085152b301e0dbc8af7646e
Tyler <tyguyreeses@gmail.com>
2025-02-28 22:59:09 -0700

    Allowing GameData record class to be reinitialized

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -2,4 +2,14 @@ package model;
 
 import chess.ChessGame;
 
-public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {}
+public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    public GameData withWhiteUser(String username) {
+        return new GameData(gameID, username, blackUsername, gameName, game);
+    }
+    public GameData withBlackUser(String username) {
+        return new GameData(gameID, whiteUsername, username, gameName, game);
+    }
+    public GameData withNewData(ChessGame chessGame) {
+        return new GameData(gameID, whiteUsername, blackUsername, gameName, chessGame);
+    }
+}
