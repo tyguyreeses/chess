a3e2bd2f17c908f7ce5c6a68878c84369a22a44b
Tyler <tyguyreeses@gmail.com>
2025-03-11 20:36:09 -0600

    Finish converting DataAccess into an interface

--- server/src/test/java/dataaccess/MemoryDataAccessTests.java
+++ server/src/test/java/dataaccess/MemoryDataAccessTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class DataAccessTests {
+public class MemoryDataAccessTests {
 
     private MemoryDataAccess memoryDataAccess;
 
