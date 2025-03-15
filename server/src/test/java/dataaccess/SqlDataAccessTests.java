d95a5fe5c0419205ed077b7f2678c5e01d50d226
Tyler <tyguyreeses@gmail.com>
2025-03-14 22:38:32 -0600

    Adding getUser and creatAuth tests

--- server/src/test/java/dataaccess/SqlDataAccessTests.java
+++ server/src/test/java/dataaccess/SqlDataAccessTests.java
@@ -1,7 +1,9 @@
 package dataaccess;
 
 import exception.ResponseException;
+import model.AuthData;
 import model.UserData;
+import org.eclipse.jetty.server.Authentication;
 import org.junit.jupiter.api.*;
 
 import java.sql.*;
@@ -61,26 +63,69 @@ public class SqlDataAccessTests {
              }
     }
 
-    @Test
-    public void testRegisterUserAdded() throws ResponseException {
-        UserData newUser = new UserData("newUser", "123", "test@mail.com");
-        db.createUser(newUser);
-        assertNotNull(db.getUser("newUser"));
+    /**
+     * register Tests
+     */
+    @Nested
+    class RegisterTests {
+        @Test
+        public void testRegisterUserAdded() throws ResponseException {
+            UserData newUser = new UserData("newUser", "123", "test@mail.com");
+            db.createUser(newUser);
+            assertNotNull(db.getUser("newUser"));
+        }
+
+        @Test
+        public void testRegisterUserMultipleTimes() throws ResponseException {
+            UserData newUser = new UserData("testuser", "123", "test@mail.com");
+            ResponseException ex = assertThrows(ResponseException.class, () -> db.createUser(newUser));
+            assertEquals(403, ex.statusCode());
+        }
+
+        @Test
+        public void testRegisterUserBadRequest() {
+            UserData newUser = new UserData("", "", null);
+            ResponseException ex = assertThrows(ResponseException.class, () -> db.createUser(newUser));
+            assertEquals(400, ex.statusCode());
+        }
     }
 
-    @Test
-    public void testRegisterUserMultipleTimes() throws ResponseException {
-        UserData newUser = new UserData("testuser", "123", "test@mail.com");
-        db.createUser(newUser);
-        ResponseException ex = assertThrows(ResponseException.class, () -> db.createUser(newUser));
-        assertEquals(403, ex.statusCode());
+    /**
+     * getUser Tests
+     */
+    @Nested
+    class GetUserTests {
+        @Test
+        public void testGetUserSuccess() throws ResponseException {
+            UserData expected = new UserData("testUser", "testPassword", "testEmail");
+            UserData actual = db.getUser("testUser");
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void testGetUserNotExist() throws ResponseException {
+            assertNull(db.getUser("badUser"));
+        }
+
+        @Test
+        public void testGetUserBadRequest() {
+            assertThrows(ResponseException.class, () -> db.getUser(null));
+        }
     }
 
-    @Test
-    public void testRegisterUserBadRequest() {
-        UserData newUser = new UserData("", "", null);
-        ResponseException ex = assertThrows(ResponseException.class, () -> db.createUser(newUser));
-        assertEquals(400, ex.statusCode());
+    /**
+     * auth Tests
+     */
+    @Nested
+    class AuthTests {
+        @Test
+        public void testCreateAuth() throws ResponseException {
+            UserData ud = new UserData("authTest", "authPassword", "auth@mail.com");
+            db.createUser(ud);
+            String auth = db.createAuth("authTest");
+            AuthData authData = new AuthData(auth, "authTest");
+            assertEquals(authData, db.getAuth(auth));
+        }
     }
 
     @Test
