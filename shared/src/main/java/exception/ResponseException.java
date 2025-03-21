58ec3dce67f7bdee34eec7030b26a98d57b7ba36
Tyler <tyguyreeses@gmail.com>
2025-03-20 21:44:01 -0600

    Adding ServerFacade frame from petshop

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -2,6 +2,9 @@ package exception;
 
 import com.google.gson.Gson;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ResponseException extends Exception {
@@ -16,6 +19,12 @@ public class ResponseException extends Exception {
         return new Gson().toJson(Map.of("message", getMessage(), "status", statusCode));
     }
 
+    public static ResponseException fromJson(InputStream stream) {
+        var map = new Gson().fromJson(new InputStreamReader(stream), HashMap.class);
+        var status = ((Double)map.get("status")).intValue();
+        String message = map.get("message").toString();
+        return new ResponseException(status, message);
+    }
 
     public int statusCode() {
         return statusCode;
