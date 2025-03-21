03518a69323c9df626629da7dbc65895bbb63907
Tyler <tyguyreeses@gmail.com>
2025-03-21 17:13:23 -0600

    Adding tests for clearData and registerUser

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,5 +1,8 @@
 package client;
 
+import exception.ResponseException;
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
 import ui.ServerFacade;
@@ -8,13 +11,14 @@ import ui.ServerFacade;
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
-        ServerFacade facade = new ServerFacade(port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -22,10 +26,30 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    public void cleanUp() throws ResponseException {facade.clearData();}
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void testClearData() {
+        Assertions.assertDoesNotThrow(() -> facade.clearData());
     }
 
+    @Test
+    public void testRegisterUser() throws ResponseException {
+        AuthData expected = new AuthData(null, "testUser");
+        UserData user = new UserData("testUser", "password", "email");
+        AuthData actual = facade.registerUser(user);
+        Assertions.assertEquals(expected.username(), actual.username());
+    }
+
+    @Test
+    public void testRegisterUserTwice() throws ResponseException {
+        UserData user = new UserData("testUser", "password", "email");
+        facade.registerUser(user);
+        try {
+            facade.registerUser(user);
+        } catch (ResponseException e) {
+            Assertions.assertEquals(403, e.statusCode());
+        }
+    }
 }
