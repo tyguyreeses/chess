87016ae18e599139ff2c0545cdd7384e45bc901b
Tyler <tyguyreeses@gmail.com>
2025-03-23 14:31:21 -0600

    Implementing preloginRepl from main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import chess.*;
 
+import ui.repls.PreLoginRepl;
+
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        new PreLoginRepl(8080).run();
     }
 }
