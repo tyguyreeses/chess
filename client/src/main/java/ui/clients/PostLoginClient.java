b75256744c0f86a5e5f6c76361405cee3db7f643
Tyler <tyguyreeses@gmail.com>
2025-03-24 15:55:41 -0600

    Implementing skeleton of postlogin repl and client classes

--- client/src/main/java/ui/clients/PostLoginClient.java
+++ client/src/main/java/ui/clients/PostLoginClient.java
@@ -1,4 +1,61 @@
 package ui.clients;
 
+import exception.ResponseException;
+import model.UserData;
+import ui.ServerFacade;
+
+import java.util.Arrays;
+
 public class PostLoginClient {
+    private final ServerFacade facade;
+    private final String username;
+
+
+    public PostLoginClient(ServerFacade serverFacade, String username) {
+        this.facade = serverFacade;
+        this.username = username;
+
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd.toLowerCase()) {
+                case "l", "list" -> list();
+                case "c", "create" -> create(params);
+                case "j", "join" -> join(params);
+                case "w", "watch" -> watch(params);
+                case "logout" -> logout();
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String list() throws ResponseException {
+            return facade.listGames();
+    }
+
+    public String create(String... params) throws ResponseException {
+        if (params.length == 3) {
+            facade.registerUser(new UserData(params[0], params[1], params[2]));
+            return String.format("Signed in as %s", params[0]);
+        }
+        throw new ResponseException(400, "Expected format: <USERNAME> <PASSWORD> <EMAIL>");
+    }
+
+    public String help() {
+        return """
+                Options:
+                List current games: "l", "list"
+                Create a new game: "c", "create" <GAME NAME>
+                Join a game: "j", "join" <GAME ID> <COLOR>
+                Watch a game: "w", "watch" <GAME ID>
+                Logout: "logout"
+                View commands: "h", "help"
+                """;
+    }
 }
--- client/src/main/java/ui/repls/PostLoginRepl.java
+++ client/src/main/java/ui/repls/PostLoginRepl.java
@@ -1,4 +1,42 @@
 package ui.repls;
 
+import ui.ServerFacade;
+import ui.clients.PostLoginClient;
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+import static ui.EscapeSequences.SET_TEXT_COLOR_GREEN;
+
 public class PostLoginRepl {
+        private final PostLoginClient client;
+
+        public PostLoginRepl(ServerFacade serverFacade) {
+            client = new PostLoginClient(serverFacade);
+        }
+
+        public void run() {
+            System.out.println();
+            System.out.print(client.help());
+
+            Scanner scanner = new Scanner(System.in);
+            var result = "";
+            while (!result.equals("quit")) {
+                printPrompt();
+                String line = scanner.nextLine();
+
+                try {
+                    result = client.eval(line);
+                    System.out.print(SET_TEXT_COLOR_BLUE + result);
+                } catch (Throwable e) {
+                    var msg = e.toString();
+                    System.out.print(SET_TEXT_COLOR_RED + msg);
+                }
+            }
+            System.out.println();
+        }
+
+        private void printPrompt() {
+            System.out.print("\n" + RESET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_GREEN);
+        }
 }
