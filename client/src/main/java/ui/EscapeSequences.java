f41afa6c78cd061357d31395d6391afaf315b251
Tyler <tyguyreeses@gmail.com>
2025-03-24 20:40:11 -0600

    Simplifying piece names

--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -47,18 +47,18 @@ public class EscapeSequences {
     public static final String SET_BG_COLOR_WHITE = SET_BG_COLOR + "15m";
     public static final String RESET_BG_COLOR = UNICODE_ESCAPE + "[49m";
 
-    public static final String WHITE_KING = " ♔ ";
-    public static final String WHITE_QUEEN = " ♕ ";
-    public static final String WHITE_BISHOP = " ♗ ";
-    public static final String WHITE_KNIGHT = " ♘ ";
-    public static final String WHITE_ROOK = " ♖ ";
-    public static final String WHITE_PAWN = " ♙ ";
-    public static final String BLACK_KING = " ♚ ";
-    public static final String BLACK_QUEEN = " ♛ ";
-    public static final String BLACK_BISHOP = " ♝ ";
-    public static final String BLACK_KNIGHT = " ♞ ";
-    public static final String BLACK_ROOK = " ♜ ";
-    public static final String BLACK_PAWN = " ♟ ";
+    public static final String W_KING = " ♔ ";
+    public static final String W_QUEEN = " ♕ ";
+    public static final String W_BISHOP = " ♗ ";
+    public static final String W_KNIGHT = " ♘ ";
+    public static final String W_ROOK = " ♖ ";
+    public static final String W_PAWN = " ♙ ";
+    public static final String B_KING = " ♚ ";
+    public static final String B_QUEEN = " ♛ ";
+    public static final String B_BISHOP = " ♝ ";
+    public static final String B_KNIGHT = " ♞ ";
+    public static final String B_ROOK = " ♜ ";
+    public static final String B_PAWN = " ♟ ";
     public static final String EMPTY = " \u2003 ";
 
     public static String moveCursorToLocation(int x, int y) { return UNICODE_ESCAPE + "[" + y + ";" + x + "H"; }
