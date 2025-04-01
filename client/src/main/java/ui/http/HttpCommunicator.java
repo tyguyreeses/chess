ae8765d325500d2b7661bf5ddf29b020fefaf136
Tyler <tyguyreeses@gmail.com>
2025-03-31 18:51:28 -0600

    Adding class framework to implement websocket

---
+++ client/src/main/java/ui/http/HttpCommunicator.java
@@ -0,0 +1,10 @@
+package ui.http;
+
+/*
+
+separate out the http handling methods from the server facade class to mirror
+
+ */
+
+public class HttpCommunicator {
+}
---
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -0,0 +1,12 @@
+package websocket;
+
+/*
+
+Tracks which connections are still active
+Needs to function correctly when multiple games are running concurrently
+    - ex: notifications from one game don't get sent to the others
+
+ */
+
+public class ConnectionManager {
+}
