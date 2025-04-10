228f319e00f5a5d3487ed8a09f674d337f6669f2
Tyler <tyguyreeses@gmail.com>
2025-04-09 20:02:35 -0600

    Make small formatting fixes

--- client/src/main/java/ui/websocket/ServerMessageObserver.java
+++ client/src/main/java/ui/websocket/ServerMessageObserver.java
@@ -2,11 +2,6 @@ package ui.websocket;
 
 import websocket.messages.ServerMessage;
 
-/*
-
-the GameRepl object will implement this class, since it is the only one that needs to utilize websocket
-
- */
 public interface ServerMessageObserver {
     void notify(ServerMessage serverMessage);
 }
