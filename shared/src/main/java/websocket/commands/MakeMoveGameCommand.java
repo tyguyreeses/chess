7688647b66c718a66c9f79d310e83e45d1beb233
Tyler <tyguyreeses@gmail.com>
2025-04-10 21:19:47 -0600

    Fix type

--- shared/src/main/java/websocket/commands/MakeMoveGameCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveGameCommand.java
@@ -7,7 +7,7 @@ public class MakeMoveGameCommand extends UserGameCommand {
     private final ChessMove move;
 
     public MakeMoveGameCommand(String authToken, Integer gameID, ChessMove move) {
-        super(CommandType.CONNECT, authToken, gameID);
+        super(CommandType.MAKE_MOVE, authToken, gameID);
         this.move = move;
     }
 
